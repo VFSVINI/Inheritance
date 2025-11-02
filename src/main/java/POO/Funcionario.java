ffed55d503261aeef2f3632be614cb7e3f1d2cc0
Vinícius França <vfs2@cin.ufpe.br>
2025-11-02 02:33:32 -0300

    Introdução Ao Estudo De Herança/Polimorfismo

--- src/main/java/POO/Funcionario.java
+++ src/main/java/POO/Funcionario.java
@@ -7,4 +7,61 @@ public class Funcionario extends Pessoa {
     String cargo;
 
 
+    //Polimorfismo
+    //Sobrecrita @Override
+
+
+    public void setCpf(String cpf) {
+
+        this.cpf = cpf;
+    }
+
+    public void setSalario(double salario) {
+
+        this.salario = salario;
+    }
+
+    public void setCargo(String cargo) {
+
+        this.cargo = cargo;
+    }
+
+    @Override
+    public void apresentar() {
+        super.apresentar();
+        System.out.println(cpf);
+        System.out.println(salario);
+        System.out.println(cargo);
+
+    }
+
+    //Sobrecarga
+    public void almocar(){
+
+    }
+
+    public void almocar(int opcao){
+
+
+    }
+
+    public void almocar(String opcao){
+
+
+    }
+
+    public void almocar(String texto, int opcao){
+
+
+    }
+
+    public void almocar(String texto, int opcao, double formaPagamento){
+
+
+    }
+
+    public void almocar(int opcao, String texto, double formaPagamento){
+
+
+    }
 }
--- src/main/java/POO/Main.java
+++ src/main/java/POO/Main.java
@@ -2,5 +2,15 @@ package POO;
 public class Main {
     public static void main(String[] args) {
 
+        Funcionario f = new Funcionario();
+
+        f.setNome("Vini");
+        f.setIdade(23);
+        f.setSexo('M');
+        f.setCpf("121.683.872-33");
+        f.setCargo("Arquiteto de Software");
+        f.setSalario(25.000);
+
+        f.apresentar();
     }
 }
